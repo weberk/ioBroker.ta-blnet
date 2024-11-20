83aac3b756a27356448a611d218dbc5cf5c8dd58
Klaus Weber <klausatweberesprit@gmail.com>
2024-11-20 14:26:58 +0100

    reindented

--- doc/dl-lesen.c
+++ doc/dl-lesen.c
@@ -32,6 +32,7 @@
  *                  $Id$       *
  *****************************************************************************/
 #include <sys/types.h>
+
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
