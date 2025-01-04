f5a02c54258bb127157aeb210e0f33ba27064c90
Klaus Weber <klausatweberesprit@gmail.com>
2025-01-04 12:33:47 +0100

    fixed mocha-setup

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -5,10 +5,12 @@ process.on("unhandledRejection", (e) => {
 
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
-const sinonChai = require("sinon-chai");
-const chaiAsPromised = require("chai-as-promised");
-const { should, use } = require("chai");
+(async () => {
+    const sinonChai = await import("sinon-chai");
+    const chaiAsPromised = await import("chai-as-promised");
+    const { should, use } = await import("chai");
 
-should();
-use(sinonChai);
-use(chaiAsPromised);
+    should();
+    use(sinonChai.default);
+    use(chaiAsPromised.default);
+})();
